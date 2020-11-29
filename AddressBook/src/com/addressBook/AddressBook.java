9c12eddf5698d9b86f407f24129a205f751cd224
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-11-29 20:07:21 +0530

    Added: Delete Contact Method

--- AddressBook/src/com/addressBook/AddressBook.java
+++ AddressBook/src/com/addressBook/AddressBook.java
@@ -9,9 +9,11 @@ import com.addressBook.model.Person;
 public class AddressBook {
 	
 	public static List<Person> personList = new ArrayList<Person>();
-	Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
 	
-	public void addPerson()
+	static int choice =0;
+	
+	private void addPerson()
 	{
 		System.out.println("Enter Person Details");
 		System.out.println("Enter FirstName: ");
@@ -29,15 +31,107 @@ public class AddressBook {
 		System.out.println("Enter zipCode: ");
 		String zipCode = sc.next();
 		
-		Person person1 = new Person(firstName, lastName, address, city, state, mobileNum, zipCode);
-		personList.add(person1);
+		Person person = new Person(firstName, lastName, address, city, state, mobileNum, zipCode);
+		personList.add(person);
 		System.out.println("Person details added successfully");
 	}
 	
+	private void editContact() {
+		if(personList.isEmpty()) {
+			System.out.println("There are no contacts to print");
+		} else {
+			String address,city,state,mobileNum,zipCode;
+			int id;
+			for(Person contact : personList) {
+				System.out.println("ID "+personList.indexOf(contact)+":\n"+contact);
+			}
+			System.out.println("Enter ID of contact to edit: ");
+			id = sc.nextInt();
+			System.out.println(personList.get(id));
+			System.out.println("Please select the option to edit\n1.Address\n2.City\n3.State\n4.zipCode\n5.Mobile Number");
+			choice = sc.nextInt();
+			switch(choice) {
+			case 1:
+				System.out.println("Enter Address: ");
+				address = sc.nextLine();
+				personList.get(id).setAddress(address);
+				break;
+			case 2:
+				System.out.println("Enter City: ");
+				city = sc.nextLine();
+				personList.get(id).setCity(city);
+				break;
+			case 3:
+				System.out.println("Enter State: ");
+				state = sc.nextLine();
+				personList.get(id).setState(state);
+				break;
+			case 4:
+				System.out.println("Enter Zip Code: ");
+				zipCode = sc.nextLine();
+				personList.get(id).setZipCode(zipCode);
+				break;
+			case 5:
+				System.out.println("Enter Mobile Number: ");
+				mobileNum = sc.nextLine();
+				personList.get(id).setMobileNum(mobileNum);
+				break;
+			default:
+				System.out.println("Error!! Choose correct Option");
+				editContact();
+			}
+		}
+	}
+
+	private void printContact() {
+		if(personList.isEmpty()){
+	         System.out.println("There are no contacts to print");
+	    } else {
+	         for(Person contact : personList){
+	            System.out.println(contact);
+	         }
+	     }
+	}
+
+	private void deleteContact() {
+		if(personList.isEmpty()){
+			System.out.println("There are no contacts to delete in the addressbook");
+		} else {
+			System.out.println("Enter firstname to delete the person");
+			String firstName = sc.nextLine();
+			for(int count=0;count < personList.size(); count++){
+				if(personList.get(count).getFirstName().equals(firstName)){
+					personList.remove(personList.get(count));
+				}
+			}
+		}
+	}
+
 	public static void main(String args[]) {
-		System.out.println("*****WELCOME TO ADDRESS BOOK PROGRAM*****");
 		AddressBook contact = new AddressBook();
-		contact.addPerson();
-	}	
+		System.out.println("*****WELCOME TO ADDRESS BOOK PROGRAM*****");
+		while(choice <= 5) {
+			System.out.println("1.Add Person\n2.Print contact details\n3.Edit contact details\n4.Delete contact details\n5.Exit ");
+			choice = sc.nextInt();
+			switch(choice) {
+			case 1:
+				contact.addPerson();
+				break;
+			case 2:
+				contact.printContact();
+				break;
+			case 3:
+				contact.editContact();
+				break;
+			case 4:
+				contact.deleteContact();
+			case 5:
+				System.exit(0);
+			default:
+				System.out.println("Error! Choose right option from the above given options Only");
+			}
+		}
+	}
+	
 }
 
